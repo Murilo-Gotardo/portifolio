cc1195181450694a0568a5888786740d5f5dc68f
Murilo Gotardo <murilogotardo08@gmail.com>
2023-03-06 18:24:57 -0300

    Layout improvement

--- js/app.js
+++ js/app.js
@@ -16,3 +16,10 @@ const titulo2 = document.querySelector('.escreve2');
 
 typeWriter(titulo2)
 
+const titulo3 = document.querySelector('.escreve3');
+
+typeWriter(titulo3)
+
+const titulo4 = document.querySelector('.escreve4');
+
+typeWriter(titulo4)
